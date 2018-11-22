f42ba289a9795a7cf984f9f221c1cf729bf3649e
YsoSirius <kona1@gmx.at>
2018-11-22 20:07:48 +0100

    back to normal

--- R/render-tree.R
+++ R/render-tree.R
@@ -9,25 +9,25 @@
 #' is useful if you want to save an expression in a variable.
 #' @seealso \code{\link{shinyTree}}
 #' @export
-# renderTree <- function(expr, env = parent.frame(), quoted = FALSE){
-#   func <- shiny::exprToFunction(expr, env, quoted)
-#   return(function(shinysession, name, ...) {
-#     tree <- func()
-#     if(inherits(expr, "Node")){
-#       shiny::HTML(as.character(datatreeToTags(tree)))  
-#     } else {
-#       shiny::HTML(as.character(listToTags(tree)))  
-#     }
-#   })
-# }
 renderTree <- function(expr, env = parent.frame(), quoted = FALSE){
   func <- shiny::exprToFunction(expr, env, quoted)
   return(function(shinysession, name, ...) {
     tree <- func()
-    updateTree(shinysession,name,tree) 
-    NULL
+    if(inherits(expr, "Node")){
+      shiny::HTML(as.character(datatreeToTags(tree)))
+    } else {
+      shiny::HTML(as.character(listToTags(tree)))
+    }
   })
 }
+# renderTree <- function(expr, env = parent.frame(), quoted = FALSE){
+#   func <- shiny::exprToFunction(expr, env, quoted)
+#   return(function(shinysession, name, ...) {
+#     tree <- func()
+#     updateTree(shinysession,name,tree) 
+#     NULL
+#   })
+# }
 
 #' Render an empty ShinyTree
 #' 
